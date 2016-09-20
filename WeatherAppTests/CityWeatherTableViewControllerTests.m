03980e8012549ec797d3ec2a6525bfd978b77c0e
Robert Shaw <robert@glucode.com>
2016-09-20 15:15:46 +0200

    Added OHHTStubs to podfile and stub request in test
    Signed-off-by: Robert Shaw <robert@glucode.com>

--- WeatherAppTests/CityWeatherTableViewControllerTests.m
+++ WeatherAppTests/CityWeatherTableViewControllerTests.m
@@ -1,6 +1,8 @@
 #import <XCTest/XCTest.h>
 #import "CityWeatherTableViewController.h"
 #import "CityWeatherTableViewController_TestingPrivates.h"
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHHTTPStubs/OHPathHelpers.h>
 
 @interface CityWeatherTableViewControllerTests : XCTestCase
 
@@ -12,8 +14,18 @@
 
 - (void)testThatItPopulatesResponseData {
     // given
-    self.cityWeatherTableViewController = [[CityWeatherTableViewController alloc] initWithNibName:nil bundle:nil];
+    NSString *cityName = @"Pretoria";
+    self.cityWeatherTableViewController = [[CityWeatherTableViewController alloc] initWithCity:cityName];
     
+    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
+        if ([request.URL.absoluteString isEqualToString:[NSString stringWithFormat:@"http://api.worldweatheronline.com/premium/v1/weather.ashx?q=%@&format=json&num_of_days=5&key=fc5d2e78218d43dfac4142824160609", cityName]]) {
+            return YES;
+        }
+        return NO;
+    } withStubResponse:^OHHTTPStubsResponse * _Nonnull(NSURLRequest * _Nonnull request) {
+        return [OHHTTPStubsResponse responseWithFileAtPath:OHPathForFile(@"weatherResponse.json", self.class) statusCode:200 headers:nil];
+    }];
+        
     // when
     [self.cityWeatherTableViewController view];
     [self.cityWeatherTableViewController connectionDidFinishLoading:nil];
